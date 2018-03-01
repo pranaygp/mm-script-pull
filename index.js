88fd45f50d0ab6d41bf284e22c455104cb03fdd1
Pranay Prakash <pranay.gp@gmail.com>
2018-03-01 02:15:25 -0600

    return early if sending error

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const { promisify } = require('util')
 const mongoose = require('mongoose')
 const AWS = require('aws-sdk')
 const uuid = require('node-uuid')
+const authenticate = require('mm-authenticate')(mongoose)
 const { Team, Script } = require('mm-schemas')(mongoose)
 const { send, buffer } = require('micro')
-const authenticate = require('mm-authenticate')(mongoose)
 
 mongoose.connect(process.env.MONGO_URL)
 mongoose.Promise = global.Promise
@@ -19,8 +19,7 @@ const getObject = promisify(s3.getObject.bind(s3))
 module.exports = authenticate(async (req, res) => {
   const urlParams = req.url.split('/')
   if(urlParams.length !== 2) {
-    send(res, 400, 'Malformed URL')
-    return
+    return send(res, 400, 'Malformed URL')
   }
   const [_, name] = urlParams
 
@@ -28,14 +27,13 @@ module.exports = authenticate(async (req, res) => {
   const team = await Team.findOne({name}).populate('latestScript').exec()
 
   if(!team) {
-    send(res, 404, `Team ${name} not found`)
-    return;
+    return send(res, 404, `Team ${name} not found`)
   }
   if(!team.latestScript) {
-    send(res, 404, `No script uploaded for ${name}`)
+    return send(res, 404, `No script uploaded for ${name}`)
   }
   if(!team.latestScript.canBeAccessedBy(req.user)) {
-    send(res, 401, 'Unauthorized')
+    return send(res, 401, 'Unauthorized')
   }
 
   // Get file from s3
